b01a1e772d9cb4d64de7f4de2650ee84b96fd6e6
test <test@eefung.com>
2017-07-23 22:02:25 +0800

    Add struceture

---
+++ go-demo-1/src/demo1/structure/ftoc.go
@@ -0,0 +1,18 @@
+
+package main
+
+import "fmt"
+
+func main(){
+const freezingF ,boilingF = 32.0 ,212.0
+
+fmt.Printf("freezingF %g",ftoc(freezingF))
+fmt.Printf("boilingF %g",ftoc(boilingF))
+
+
+}
+
+func ftoc(f float64) float64{
+   return (f-32)*5/9
+
+}
