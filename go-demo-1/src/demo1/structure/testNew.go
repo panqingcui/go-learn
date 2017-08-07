daa5421e5c805dcee45e9b7caf83d9e287de5695
panqingcui <panqingcui@eefung.com>
2017-08-07 18:00:09 +0800

    类型提交

--- go-demo-1/src/demo1/structure/testNew.go
+++ go-demo-1/src/demo1/structure/testNew.go
@@ -9,7 +9,7 @@ func main(){
   fmt.Println("*p",*p)
   fmt.Println("newInt()",*newInt()) 
   v:=1
-  fmt.Println("int1",*newInt1())
+  fmt.Println("int1",newInt1())
  fmt.Println("v",v)
  fmt.Println("incr",*incr(&v))
 }
@@ -17,9 +17,9 @@ func main(){
 func  newInt() *int{
    return new(int)
 }
-func   newInt1() *int{
+func   newInt1() int{
   var  dummy int
-  return &dummy
+  return dummy
 }
 
 func incr(p *int) *int{
