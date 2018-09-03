a8bed45bc9855a22c1edac0034c1b3d8c91c395b
panqingcui <panqingcui@eefung.com>
2018-09-03 11:36:10 +0800

    [Modify] func

--- go-demo-1/src/function/func.go
+++ go-demo-1/src/function/func.go
@@ -19,7 +19,6 @@ func main() {
 	fmt.Printf("max %d \n", max(1, 2))
 	a := "f"   
 	b := "s"
-
 	x, y := swap.Swap(a, b)
 	fmt.Printf("swap %s,%s", x, y)
 	var balance = [5]float32{1000.0, 2.0, 3.4, 7.0, 50.0}
