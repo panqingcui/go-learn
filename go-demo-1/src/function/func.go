41957ce332e363e9792f5dcfd3af100644c67bdc
panqingcui <panqingcui@eefung.com>
2018-09-30 13:33:35 +0800

    [Modify] time

--- go-demo-1/src/function/func.go
+++ go-demo-1/src/function/func.go
@@ -13,17 +13,21 @@ func max(a, b int) int {
 	}
 	return b
 }
-
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
 func main() {
 	fmt.Printf("max %d \n", max(1, 2))
 	fmt.Printf("max %d \n", max(1, 2))
-	a := "f"   
+	a := "f"
 	b := "s"
 	x, y := swap.Swap(a, b)
 	fmt.Printf("swap %s,%s", x, y)
 	var balance = [5]float32{1000.0, 2.0, 3.4, 7.0, 50.0}
 
 	for i := 0; i < len(balance); i++ {
-		fmt.Printf(" balance[%d]|%f \n", i, balance[i])
 	}
 }
